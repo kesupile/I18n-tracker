17e00e8cfa618285d275959ae249c6b2d915f80d
Kesupile <kesupilek@googlemail.com>
2017-08-11 16:21:46 +0100

    syntax error

--- bin/track.bin.js
+++ bin/track.bin.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 console.log('We are ready to track your package!!');
-const processDir = process.cwd());
+const processDir = process.cwd();
 const configDir = `processDir/i18n-tracker.config.json`
 const config = require(configDir)
 console.log(config)
