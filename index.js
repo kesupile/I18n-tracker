57a0b6526881439beb90d718f4db8465df0d3a98
Kesupile <kesupilek@googlemail.com>
2017-08-12 13:16:17 +0100

    - defining the main exports for the module: getTranslations

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('We are in the index')
+const fs = require('fs')
+
+const getLanguages = require('./private/getLanguages')
+const checkExistance = require('./private/checkExistance')
+const getThisDir = require('./private/getThisDir')
+
+const thisDir = getThisDir(module.parent.filename)
+const configDir = './i18n-tracker.config.json'
+const config = JSON.parse(fs.readFileSync(configDir, 'utf8'))
+
+
+function logThisDir() {
+  console.log(thisDir);
+}
+
+function getTranslations() {
+  const translations = getLanguages(config.translations)
+
+  //check the existance of translation files
+  checkExistance(thisDir, translations)
+}
+
+
+module.exports = {
+  logThisDir: logThisDir,
+  getTranslations: getTranslations
+}
