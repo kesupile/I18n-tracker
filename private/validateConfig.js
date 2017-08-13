0e5dd64cd1795708f4a6f8b0fbd21c1cf0255b44
Kesupile <kesupilek@googlemail.com>
2017-08-13 21:01:04 +0100

    - function that validates the config file

---
+++ private/validateConfig.js
@@ -0,0 +1,16 @@
+const _ = require('lodash')
+
+module.exports = function(config) {
+
+  console.log('[VALIDATECONFIG]');
+
+  if(!config.base || !Array.isArray(config.base)){
+    throw new Error('i18n-tracker.config.json requires a base property of type Array')
+  }
+
+  if(!config.translations || _.size(config.translations) < 1){
+    throw new Error('i18n-tracker.config.json requires a translation property with at least one entry')
+  }
+
+  return true
+}
