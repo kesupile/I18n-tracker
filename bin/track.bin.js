284879acdcf41c8a978f31154ddee1b24c56c1f9
Kesupile <kesupilek@googlemail.com>
2017-08-13 20:58:43 +0100

    - testing colors package - throwing custom error if config does not exist - using validateEnvironment to optimise

--- bin/track.bin.js
+++ bin/track.bin.js
@@ -1,13 +1,27 @@
 #!/usr/bin/env node
 
 const fs = require('fs')
+const colors = require('colors')
+
+const validateEnvironment = require('../private/validateEnvironment')
+const getLanguages = require('../private/getLanguages')
 
 //const processDir = process.cwd() //needed for node package
 // const configDir = `${processDir}/i18n-tracker.config.json`
+const processDir = '../../test-client/translations' //temporary for testing
 const configDir = '../../test-client/translations/i18n-tracker.config.json' //temporary for testing
-console.log(configDir)
 
 
-const config = JSON.parse(fs.readFileSync(configDir, 'utf8'))
+let config
+try {
+  config = JSON.parse(fs.readFileSync(configDir, 'utf8'))
+} catch (e) {
+  throw new Error('i18n-tracker.config.json missing')
+}
 console.log(config)
-console.log(config.testproperty)
+
+//validate the environment
+const environmentValid = validateEnvironment(processDir, config)
+console.log(`environmentValid: ${environmentValid}`);
+
+//map out the structure of the base file
