f419041374ef41fe6821e5fadbc0a6c340327100
Kesupile <kesupilek@googlemail.com>
2017-08-18 14:16:09 +0100

    - create omissions object that tracks and logs the omissions in the object

---
+++ model/Ommissions.js
@@ -0,0 +1,28 @@
+const colors = require('colors')
+const _ = require('lodash')
+
+function Omissions(root){
+  if(!root || typeof root !== 'string'){
+    throw new Error('Ommisions constructor requires a root property of type String')
+  }
+  this.root = root
+  this.omissions = new Array(0)
+}
+
+Omissions.prototype.add = function (omission) {
+  this.omissions.push(omission)
+}
+
+Omissions.prototype.logOmissions = function() {
+  let omString = ''
+  _.forEach(this.omissions, (omission) => {
+    omString += `   -- ${omission} \n`
+  })
+  console.log(
+    `  ${String(this.root).red} \n${String(omString).yellow}`
+  )
+}
+
+module.exports = Omissions
+
+const myOmission = new Omissions()
