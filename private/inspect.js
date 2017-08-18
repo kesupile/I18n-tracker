7118b8d88d2806445da7c787992a4beff9cc9e02
Kesupile <kesupilek@googlemail.com>
2017-08-18 17:32:50 +0100

    - moving inspect to to a descrete function - editing the omissions object if properties are missing - logging the omissions after inspectino

--- private/inspect.js
+++ private/inspect.js
@@ -1,17 +1,28 @@
 const _ = require('lodash')
 
 const compare = require('../private/compare')
+
 const Omissions = require('../model/Omissions')
 
-module.exports = (baseObj, root, translationObj) => {
+const inspect = function(baseObj, root) {
   let omissions = new Omissions(root)
+
   _.forEach(baseObj, (val, key) => {
     typeof val === 'string' ? omissions.addProp(key) : omissions.addInspectProp(key)
   })
-  console.log('ommisions are: ');
-  console.log(omissions.props);
+
   compare(omissions, translationObj)
-  console.log('after comparing');
-  console.log(omissions.logOmissions());
+  omissions.logOmissions()
+
+  return omissions
+}
+
+module.exports = (baseObj, root, translationObj) => {
+
+  let omissions = inspect(baseObj, root)
+
+  if(omissions.inspectProps.length > 0){
+
+  }
   //console.log(omissions.inspectProps);
 }
