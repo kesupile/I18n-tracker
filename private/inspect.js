242dd9662feb7e03ad496599f276524681085cbc
Kesupile <kesupilek@googlemail.com>
2017-08-20 19:50:20 +0100

    - preparing to create a trampoline function

--- private/inspect.js
+++ private/inspect.js
@@ -4,7 +4,13 @@ const compare = require('../private/compare')
 
 const Omissions = require('../model/Omissions')
 
-const inspect = function(baseObj, root) {
+function trampoline  (f) {
+
+}
+
+function inspect (baseObj, root, translationObj) {
+
+
   let omissions = new Omissions(root)
 
   _.forEach(baseObj, (val, key) => {
@@ -12,17 +18,29 @@ const inspect = function(baseObj, root) {
   })
 
   compare(omissions, translationObj)
-  omissions.logOmissions()
 
   return omissions
 }
 
+const doesInspectExist = function(omissions, translationObj) {
+  _.forEach(omissions.inspectProps, (value) => {
+    translationObj[value] ? omissions.addNest(value) : omissions.add(value)
+  })
+}
+
+
+
+
 module.exports = (baseObj, root, translationObj) => {
 
-  let omissions = inspect(baseObj, root)
+  let omissions = inspect(baseObj, root, translationObj)
 
   if(omissions.inspectProps.length > 0){
-
+    doesInspectExist(omissions, translationObj)
   }
+
+  omissions.logOmissions()
+  console.log(omissions.nests)
   //console.log(omissions.inspectProps);
 }
+//Documents/Orion/I18n-tracker/dev/I18n-tracker
