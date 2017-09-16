a660076cbbea06a36923a651d3144485613e8081
Kesupile <kesupilek@googlemail.com>
2017-09-16 16:31:10 +0100

    - comments to explain what needs to be done next

--- private/compare.js
+++ private/compare.js
@@ -1,8 +1,18 @@
 const _ = require('lodash')
 
 module.exports = (omissions, translationFile) => {
+
+  //check the basic props first
   _.forEach(omissions.props, (value) => {
     //check whether the prop exists
-    translationFile[value] ? null : omissions.add(value)
+    // translationFile[value] ? null : omissions.add(value)
+    _.get(translationFile, value, null) === null ? omissions.add(value) : null
   })
+
+  //check the nested props next
+
+  //1. does the parent exist? ------ THIS IS A LOOP
+    //NO ----- set all dependacies as non existant
+
+    //YES ---- inspect each dependancy
 }
