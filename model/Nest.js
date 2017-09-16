b70d0c9080b57113f118bf42c56cd8aec45f4f44
Kesupile <kesupilek@googlemail.com>
2017-09-16 16:30:01 +0100

    - added array containing the references for the child nests - added methods to add children, add props and set the full root of the object

--- model/Nest.js
+++ model/Nest.js
@@ -1,10 +1,31 @@
 function Nest(root) {
   this.root = root
   this.doesExist = null
+  this.children = new Array(0)
+  this.props = new Array(0)
+  this.omissions = new Array(0)
 }
 
 Nest.prototype.setExistance = function(bool){
   this.doesExist = bool
+
+  //if a parent doesn't exist propagate this state throughout it's children
+  if(!bool){
+    this.children.forEach((child) => {child.setExistance(false)})
+  }
+}
+
+//keep track of children so that we can alter their existance
+Nest.prototype.addChild = function(nest){
+  this.children.push(nest)
+}
+
+Nest.prototype.addProp = function(prop){
+  this.props.push(prop)
+}
+
+Nest.prototype.setFullRoot = function(root){
+  this.fullRoot = root
 }
 
 module.exports = Nest
