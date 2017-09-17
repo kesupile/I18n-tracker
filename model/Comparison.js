ec40e9d733c38b18d618078151ffa74e3b904346
Kesupile <kesupilek@googlemail.com>
2017-09-17 20:18:34 +0100

    - imported colors - isNotOmitted: check if the object being inspected has already been omitted by virtue of it being the child of an omitted parent - addOmittedNest: push the omitted nest into the array of nestOmissions [pushes the entire object without an empty omissions array along with children] - addOmittedNestProps: pushes the nest into the array of nestOmissions. The object exists but some properties are missing [pushes entire object with omissions array containing missing props] - added a method to log omissions

--- model/Comparison.js
+++ model/Comparison.js
@@ -1,12 +1,38 @@
 const
   { addOmission } = require('./PrototypeMethods')
+  require('colors')
 
 function Comparison(language){
   this.language = language
-  this.omissions = new Array(0)
-  this.nests = new Array(0)
+  this.root = language
+  this.omissions = []
+  this.nestOmissions = []
+}
+
+Comparison.prototype.isNotOmitted = function(nest){
+  //the nest doesn't exist if it is in the NestOmissions and it's state is
+  return !this.nestOmissions.find((nestOmission) => nest.id === nestOmission.id && !nestOmission.doesExist)
+}
+
+Comparison.prototype.addOmittedNest = function(nest){
+  //add the nest and its children
+  nest.pushMyselfIntoOmissions(this.nestOmissions)
+}
+
+Comparison.prototype.addOmittedNestProps = function (nest) {
+  this.nestOmissions.push(nest)
 }
 
 Comparison.prototype.addOmission = addOmission
+
+Comparison.prototype.logOmissions =  function() {
+  let omString = ''
+  this.omissions.forEach((omission) => {
+    omString += `   -- ${omission} \n`
+  })
+  console.log(
+    `  ${String(this.root).red} \n${String(omString).yellow}`
+  )
+}
 
 module.exports = Comparison
