040a3da283c7a25fb4ec5203f879e18e74d1dfb7
Kesupile <kesupilek@googlemail.com>
2017-09-17 20:27:20 +0100

    - this method is not reusable

--- model/PrototypeMethods.js
+++ model/PrototypeMethods.js
@@ -6,13 +6,3 @@ const
 module.exports.addOmission = function(prop){
   this.omissions.push(prop)
 }
-
-module.exports.logOmissions = function logOmissions() {
-  let omString = ''
-  forEach(this.omissions, (omission) => {
-    omString += `   -- ${omission} \n`
-  })
-  console.log(
-    `  ${String(this.root).red} \n${String(omString).yellow}`
-  )
-}
