89dde85e85b8e61ab52d71724ba6c6d8f548fd8c
Kesupile <kesupilek@googlemail.com>
2017-09-17 13:18:49 +0100

    - comparison is now called from track.bin.js - loop through languages and compare

--- bin/track.bin.js
+++ bin/track.bin.js
@@ -5,9 +5,11 @@ const
   validateEnvironment = require('../private/validateEnvironment'),
   getLanguages = require('../private/getLanguages'),
   inspect = require('../private/inspect'),
+  compare = require('../private/compare'),
+  { forEach } = require('lodash')
 
   //const processDir = process.cwd() //needed for node package
-  // const configDir = `${processDir}/i18n-tracker.config.json`
+  //const configDir = `${processDir}/i18n-tracker.config.json`
   processDir = '../../test-client/translations', //temporary for testing
   configDir = '../../test-client/translations/i18n-tracker.config.json' //temporary for testing
 
@@ -29,9 +31,25 @@ const baseName = config.base[0]
 const languages = getLanguages(config.translations)
 const german = require(`${processDir}/${languages[0][0]}`)
 
+console.log(languages);
 
+const omissions = inspect(baseObj, baseName)
 
+// const copy = omissions.nests.slice()
 
+// console.log('--------------slice is:-------------------');
+// console.log(JSON.stringify(copy,null,4));
 
-const omissions = inspect(baseObj, baseName, languages[0][0], german)
-omissions.logOmissions()
+require('colors') /* ---------- FOR DEBUGGING -------------- */
+
+forEach(languages, (language) => {
+
+  const langObj = require(`${processDir}/${language[0]}`)
+  compare(omissions, language[0], langObj)
+
+})
+
+console.log(JSON.stringify(omissions.comparisons, null, 4).yellow);
+
+// compare()
+//omissions.logOmissions()
