5a47b52157a7dceee62dbc5c7626594734635292
Kesupile <kesupilek@googlemail.com>
2017-09-17 12:00:37 +0100

    - refactor export

--- model/PrototypeMethods.js
+++ model/PrototypeMethods.js
@@ -2,7 +2,7 @@ const
   colours = require('colors'),
   _ = require('lodash')
 
-function logOmissions() {
+module.exports.logOmissions = function logOmissions() {
   let omString = ''
   _.forEach(this.omissions, (omission) => {
     omString += `   -- ${omission} \n`
@@ -10,8 +10,4 @@ function logOmissions() {
   console.log(
     `  ${String(this.root).red} \n${String(omString).yellow}`
   )
-}
-
-module.exports = {
-  logOmissions
 }
