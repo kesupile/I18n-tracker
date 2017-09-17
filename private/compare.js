e530992494fb03582bd78ef13da1bc0bcc05d665
Kesupile <kesupilek@googlemail.com>
2017-09-17 13:21:06 +0100

    - first check props that aren't nested

--- private/compare.js
+++ private/compare.js
@@ -1,13 +1,30 @@
-const _ = require('lodash')
+/* ------- for debugging --------------*/
 
-module.exports = (omissions, translationFile) => {
+require('colors')
 
+const { forEach } = require('lodash')
+
+
+
+
+
+module.exports = (omissions, langName, langObj) => {
+
+  const comparison = omissions.newComparison(langName)
+
+  forEach(omissions.props, (prop) => {
+    //check whether the prop exists
+    langObj[prop] ? null : comparison.addOmission(prop)
+  })
+
+
+  /*
   //check the basic props first
   _.forEach(omissions.props, (value) => {
     //check whether the prop exists
     // translationFile[value] ? null : omissions.add(value)
     _.get(translationFile, value, null) === null ? omissions.add(value) : null
-  })
+  }) */
 
   //check the nested props next
 
