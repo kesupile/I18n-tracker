6aababdd5bae9a5ef2605750c98a51376fe14668
Kesupile <kesupilek@googlemail.com>
2017-09-17 20:25:47 +0100

    - change the constructor to account for clones - flag to notifiy that the doesExist state is set as a result of a parent object missing - pushMyselfIntoOmissions: when an Nest pushes itself into a nestOmissions array its children should add themselves to the same array - clone: clone the object and it's children. If the object is called from a parent Nest then it should add itself to its parent' children array - added a method to log the omissions

--- model/Nest.js
+++ model/Nest.js
@@ -1,22 +1,36 @@
 const
   UUID = require('uuid/v4'),
-  { logOmissions } = require('./PrototypeMethods')
+  colors = require('colors'),
+  { addOmission } = require('./PrototypeMethods')
 
-function Nest(root) {
-  this.id = UUID()
-  this.root = root
+function Nest(root, cloneProps) {
+  if(!cloneProps){
+    this.id = UUID()
+    this.root = root
+  } else {
+    this.id = cloneProps.id
+    this.root = cloneProps.root
+    this.fullRoot = cloneProps.fullRoot
+  }
   this.doesExist = null
-  this.children = new Array(0)
-  this.props = new Array(0)
-  this.omissions = new Array(0)
+  this.children = []
+  this.props = []
+  this.omissions = []
+  this.parentTriggered = false
 }
 
 Nest.prototype.setExistance = function(bool){
   this.doesExist = bool
 
-  //if a parent doesn't exist propagate this state throughout it's children
-  if(!bool){
-    this.children.forEach((child) => {child.setExistance(false)})
+  // if a property object is missing, propergate this state
+  // down towards its children
+  if(bool === false){
+    this.children.forEach((child) => {
+
+      // keep track of whether it was triggered by parent
+      child.triggeredByParent()
+      child.setExistance(false)
+    })
   }
 }
 
@@ -33,6 +47,44 @@ Nest.prototype.setFullRoot = function(root){
   this.fullRoot = root
 }
 
-Nest.prototype.logOmissions = logOmissions
+Nest.prototype.triggeredByParent = function(){
+  this.parentTriggered = true
+}
+
+Nest.prototype.pushMyselfIntoOmissions = function(nestOmissions){
+  nestOmissions.push(this)
+  this.children.forEach((child) => child.pushMyselfIntoOmissions(nestOmissions))
+}
+
+Nest.prototype.clone = function(parent){
+  let clone = new Nest(null, {id: this.id, root: this.root, fullRoot: this.fullRoot})
+  parent ? parent.addChild(clone) : null
+  this.children.forEach((child) => child.clone(clone))
+  return clone
+}
+
+Nest.prototype.logOmissions = function(language){
+  const logRoot = `${language}.${this.fullRoot}`
+
+  // if triggeredByParent===false AND doesExist===true we know it is the
+  // child of a missing object, so there is no need to show it to the user
+  if(this.doesExist === false && !this.parentTriggered){
+    console.log(
+            `  ${String('Missing object: ').red}${String(logRoot).yellow}\n`
+            );
+  } else if(this.doesExist === true){
+    let omString = ''
+    this.omissions.forEach((omission) => {
+      omString += `   -- ${omission} \n`
+    })
+    console.log(
+      `  ${String(logRoot).red} \n${String(omString).yellow}`
+    )
+
+  }
+
+}
+
+Nest.prototype.addOmission = addOmission
 
 module.exports = Nest
