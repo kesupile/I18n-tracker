e70a521908c1ccc976c275d1eb33f58c281b041f
Kesupile <kesupilek@googlemail.com>
2017-09-26 22:14:53 +0100

    - check the os-locale - check if the locale is supported - set the locale of i18n

--- config/locales.js
+++ config/locales.js
@@ -1,5 +1,35 @@
-const { getTranslations } = require('../index')
+const
+  { I18n } = require('react-i18nify'),
+  osLocale = require('os-locale'),
+  fs = require('fs'),
+  { getTranslations } = require('../index'),
+  getLanguages = require('../private/getLanguages')
+  config = require('./i18n-tracker.config.json')
 
-console.log(getTranslations());
+I18n.setTranslations(getTranslations())
 
-const I18n = require('react-i18nify').I18n
+function findSupportedLocale(string){
+  const locale = string.split('_')[0]
+  //is the language supported?
+  if(config.translations[locale]){
+    return locale
+  } else { //default to english
+    return 'en'
+  }
+}
+
+module.exports = (function() {
+  return new Promise(function(resolve, reject) {
+
+    osLocale().then(locale => {
+
+      const supportedLocale = findSupportedLocale(locale)
+
+      I18n.setLocale(supportedLocale)
+      resolve(I18n)
+    }).catch(err => {
+      reject(err)
+    })
+
+  })
+})()
