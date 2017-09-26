bf76c3a31fee377174cf917790058e553e6a19f3
Kesupile <kesupilek@googlemail.com>
2017-09-26 22:13:53 +0100

    - i18n is a promise. wait for completion before doing anything

--- bin/track.bin.js
+++ bin/track.bin.js
@@ -5,39 +5,44 @@ const
   validateEnvironment = require('../private/validateEnvironment'),
   getLanguages = require('../private/getLanguages'),
   inspect = require('../private/inspect'),
-  compare = require('../private/compare')
+  compare = require('../private/compare'),
+  logError = require('../private/logError')
 
   const processDir = process.cwd()
   const configDir = `${processDir}/i18n-tracker.config.json`
   // processDir = '../../test-client/translations', //temporary for testing
   // configDir = '../../test-client/translations/i18n-tracker.config.json' //temporary for testing
 
-
 let config
-try {
-  config = JSON.parse(fs.readFileSync(configDir, 'utf8'))
-} catch (e) {
-  throw new Error('i18n-tracker.config.json missing')
-}
 
-//validate the environment
-const environmentValid = validateEnvironment(processDir, config)
+require('../config/locales').then(i18n => {
 
-const baseObj = require(`${processDir}/${config.base[0]}`)
-const baseName = config.base[0]
+    try {
+      config = JSON.parse(fs.readFileSync(configDir, 'utf8'))
+    } catch (e) {
+      throw new Error(i18n.t('errors.configMissing'))
+    }
 
+    //validate the environment
+    const environmentValid = validateEnvironment(processDir, config, i18n)
 
-const languages = getLanguages(config.translations)
-const german = require(`${processDir}/${languages[0][0]}`)
-
-const omissions = inspect(baseObj, baseName)
+    const baseObj = require(`${processDir}/${config.base[0]}`)
+    const baseName = config.base[0]
 
 
-languages.forEach((language) => {
+    const languages = getLanguages(config.translations)
+    const german = require(`${processDir}/${languages[0][0]}`)
 
-  const langObj = require(`${processDir}/${language[0]}`)
-  compare(omissions, language[0], langObj)
+    const omissions = inspect(baseObj, baseName)
 
-})
 
-omissions.logOmissions(languages)
+    languages.forEach((language) => {
+
+      const langObj = require(`${processDir}/${language[0]}`)
+      compare(omissions, language[0], langObj)
+
+    })
+
+    omissions.logOmissions(languages)
+
+}).catch(e => console.log(e))
