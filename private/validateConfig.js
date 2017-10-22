d71841c28dedbe211cbfb693af51ad7cbb89fa57
Kesupile <kesupilek@googlemail.com>
2017-10-22 14:37:29 +0100

    - ensure placeholder is of the correct type

--- private/validateConfig.js
+++ private/validateConfig.js
@@ -10,5 +10,11 @@ module.exports = function(config, i18n) {
     throw new Error(i18n.t('errors.translationsRequired'))
   }
 
+  if(config.placeholder){
+    if(typeof config.placeholder !== 'string' && config.placeholder !== true){
+      throw new Error(i18n.t('errors.placeholderWrongType'))
+    }
+  }
+
   return true
 }
